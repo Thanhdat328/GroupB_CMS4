1b0d1b20105b40e6281b7ae9dbf3a31f79f8aef1
Truongthithuyhoa <21211tt4610@mail.tdc.edu.vn>
2023-10-30 09:46:25 +0700

    Hoan thanh module(15)

--- wordpress-6.3.1/wordpress/wp-content/themes/twentytwentyone/search.php
+++ wordpress-6.3.1/wordpress/wp-content/themes/twentytwentyone/search.php
@@ -13,20 +13,20 @@ get_header();
 
 if ( have_posts() ) {
 	?>
-	<header class="page-header alignwide">
-		<h1 class="page-title">
-			<?php
+<header class="page-header alignwide">
+	<h1 class="page-title">
+		<?php
 			printf(
 				/* translators: %s: Search term. */
 				esc_html__( 'Results for "%s"', 'twentytwentyone' ),
 				'<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
 			);
 			?>
-		</h1>
-	</header><!-- .page-header -->
+	</h1>
+</header><!-- .page-header -->
 
-	<div class="search-result-count default-max-width">
-		<?php
+<div class="search-result-count default-max-width">
+	<?php
 		printf(
 			esc_html(
 				/* translators: %d: The number of search results. */
@@ -40,8 +40,8 @@ if ( have_posts() ) {
 			(int) $wp_query->found_posts
 		);
 		?>
-	</div><!-- .search-result-count -->
-	<?php
+</div><!-- .search-result-count -->
+<?php
 	// Start the Loop.
 	while ( have_posts() ) {
 		the_post();
@@ -61,5 +61,81 @@ if ( have_posts() ) {
 } else {
 	get_template_part( 'template-parts/content/content-none' );
 }
+?>
+<style>
+	ul.timeline {
+		list-style-type: none;
+		position: relative;
+	}
+
+	ul.timeline:before {
+		content: ' ';
+		background: #d4d9df;
+		display: inline-block;
+		position: absolute;
+		left: 29px;
+		width: 2px;
+		height: 100%;
+		z-index: 400;
+	}
+
+	ul.timeline>li {
+		margin: 20px 0;
+		padding-left: 20px;
+	}
+
+	ul.timeline>li:before {
+		content: ' ';
+		background: white;
+		display: inline-block;
+		position: absolute;
+		border-radius: 50%;
+		border: 3px solid #22c0e8;
+		left: 20px;
+		width: 20px;
+		height: 20px;
+		z-index: 400;
+	}
+</style>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container mt-5 mb-5">
+	<div class="row">
+		<div class="col-md-6 offset-md-3">
+			<h4>Latest News</h4>
+			<ul class="timeline">
+				<?php if (have_posts()) : ?>
+				<?php while (have_posts()) : the_post(); ?>
+				<li>
+					<a target="_blank" href="https://www.totoprayogo.com/#">
+						<?php the_title(); ?>
+					</a>
+					<a href="#" class="float-right">
+						<?php
+						$date = date('F d, Y'); // Lấy ngày tháng năm hiện tại
+						echo $date; // Hiển thị ngày tháng năm?></a>
+					<p>
+						<?php
+						$excerpt = get_the_excerpt(); // Lấy phần tóm tắt
+
+						$limited_excerpt = wp_trim_words($excerpt, 30, ''); // Giới hạn 20 từ
+
+
+						echo $limited_excerpt; // Hiển thị phần tóm tắt đã giới hạn?>....
+					</p>
+				</li>
+				<?php endwhile;?>
+				<?php endif; ?>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
+<?php
 
 get_footer();
+?>
