2c6744a7798af1b53b08d3f3a16e280b7d6168d5
TrongNe203 <trongchoigame0@gmail.com>
2023-11-01 00:10:56 +0700

    Revert "Merge branch 'Truong_Thi_Thuy_Hoa_Module(2)'"
    This reverts commit 7619bdccd4e36bec5023473277fcdc99215a3dd9, reversing
    changes made to ae233f1053cb68b34fa2cf0eeaaa52147ecef210.

--- wordpress-6.3.1/wordpress/wp-content/themes/twentytwentyone/search.php
+++ wordpress-6.3.1/wordpress/wp-content/themes/twentytwentyone/search.php
@@ -24,7 +24,6 @@ if ( have_posts() ) {
 			?>
 		</h1>
 	</header><!-- .page-header -->
-<<<<<<< HEAD
 	<style>
     #border{
         border-right:1px solid black;
@@ -80,37 +79,6 @@ if ( have_posts() ) {
 </div>
 	</div><!-- .search-result-count -->
 	<?php
-=======
-
-	<div class="search-result-count default-max-width">
-		<?php
-		printf(
-			esc_html(
-				/* translators: %d: The number of search results. */
-				_n(
-					'We found %d result for your search.',
-					'We found %d results for your search.',
-					(int) $wp_query->found_posts,
-					'twentytwentyone'
-				)
-			),
-			(int) $wp_query->found_posts
-		);
-		?>
-	</div><!-- .search-result-count -->
-	<?php
-	// Start the Loop.
-	while ( have_posts() ) {
-		the_post();
-
-		/*
-		 * Include the Post-Format-specific template for the content.
-		 * If you want to override this in a child theme, then include a file
-		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-		 */
-		get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
-	} // End the loop.
->>>>>>> Truong_Thi_Thuy_Hoa_Module(2)
 
 	// Previous/next page navigation.
 	twenty_twenty_one_the_posts_navigation();
